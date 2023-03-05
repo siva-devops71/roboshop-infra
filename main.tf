abc701eb860220307bded0d19f23a2713afa3233
Siva Kumar <sivakumar9399959803@gmail.com>
2023-03-05 13:32:09 +0530

    learn in terraform

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ module "ec2" {
   source        = "./ec2"
   component     = each.value["name"]
   instance_type = each.value["type"]
-  password      = each.value["password"]
+  password      = try(each.value["password"], null)
 }
