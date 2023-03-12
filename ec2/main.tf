db92a8c54a3c81d0af68ff2f5e0ee5a746895550
Siva Kumar <sivakumar9399959803@gmail.com>
2023-03-12 10:52:58 +0530

    learn in terraform

--- ec2/main.tf
+++ ec2/main.tf
@@ -18,7 +18,7 @@ resource "null_resource" "provisioner" {
   provisioner "remote-exec" {
     connection {
       host     = aws_instance.ec2.public_ip
-      user     = "root"
+      user     = "centos"
       password = "DevOps321"
 
   }
